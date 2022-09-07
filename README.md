46747459ee2f453dd3d3a7204f1dd97b8b08518a
creep3rcrafter <creep3rcrafter@gmail.com>
2022-09-07 01:55:37 -0600

    Template Builder
    Added Template Builder and Better readme.md

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#  Ice-Spear - a Breath of the Wild Editor
+#  Ice-Spear - a Breath of the Wild Editor - Template Builder Version
 ![alt Ice-Spear](assets/icons/icon_256_thin.png)
 
 For any information on how to use this, please see the Wiki: <br/>
@@ -7,7 +7,49 @@ https://gitlab.com/ice-spear-tools/ice-spear/wikis/home
 Issues for bugs and features are inside the issue-repo: <br/>
 https://gitlab.com/ice-spear-tools/issue-tracker/issues
 
+### Whats New
 
+- **How to use it**
+  - Added a new build template button under the Tools section.
+  - Simply select all the actors that you want.
+  - Enter the Template name just use simple characters spaces will be replaced with `_`.
+  - Then click build template .
+  - The Template will be copied to your clipboard for you to paste
+   into your templates folder at `C:\Users\user\.ice-spear\templates` in .json format.
+  - Finally once you have created your template restart ice-spear
+  
+- **Notes**
+  - The first Actor you select be the center point for the template
+  other Actors local postion will be based off the first selected.
+  - If any linked Actors werent selected they will have to be 
+  manualy removed or added in the template and will have an `Unknown` HashID.
+  - If linked Actors have been selected any refrences will be set to the correct `{ID#}` HashID.
+  - If you have issues please report them.
+
+### Project Setup
+
+- **Windows**
+  - Pay attention to the use of `nvm` and `npm`
+  - Install the latest [NVM for Windows](https://github.com/coreybutler/nvm-windows/releases)
+  - Open cmd as admin and run `nvm install 14.19.3`
+  - Then run `nvm use 14.19.3` 
+  - Next run `npm install -d node-gyp@latest`
+  - Next run `npm install --global yarn`
+  - Next either open a new CMD in the location you wish to clone Ice-Spear to, or cd to that location.
+  - Before building, `cd` into the root directory (the folder named `ice-spear` if you cloned)
+  - Now run `yarn` and your project should be set up.
+
+### Building and Running
+
+- **For all**
+  - CD to the `./ice-spear` dir that you cloned or copyed the project.
+- **Running without building**
+  - run `npm run start` and it should open the app
+- **Building Uncompressed**
+  - run `npm run unpacked` and it should build an .exe to use at `.\ice-spear\dist\win-unpacked\Ice-Spear.exe`
+- **Building Compressed Release**
+  - run `npm run dist` and it should build a compressed version for 
+  release at `.\ice-spear\dist\Ice-Spear-#.#.#-win.7z`
 
 ### License
 ___
--- apps/field_editor/app.js
+++ apps/field_editor/app.js
@@ -23,250 +23,350 @@ const TitleBG_Handler = require("./../../lib/titlebg_handler");
 const AocMainField_Handler = require("./../../lib/aoc_handler");
 
 const App_Base = requireGlobal("apps/base.js");
+const { clipboard } = require("electron");
 
 module.exports = class App extends App_Base {
-    constructor(window, args) {
-        super(window, args);
+  constructor(window, args) {
+    super(window, args);
 
-        this.isAoc = !!this.config.getValue("game.aocPath");
-        this.fieldGamePath = this.isAoc
-            ? path.join(
-                  this.config.getValue("game.aocPath"),
-                  "content",
-                  "0010",
-                  "Map",
-                  "MainField"
-              )
-            : path.join(
-                  this.config.getValue("game.updatePath"),
-                  "content",
-                  "Map",
-                  "MainField"
-              );
-        this.fieldStaticGamePath = path.join(
-            this.config.getValue("game.updatePath"),
-            "content",
-            "Physics",
-            "StaticCompound",
-            "MainField"
-        );
-
-        this.fieldDir = null;
-        this.fieldSection = null;
-
-        this.footerNode = footer.querySelector(".data-footer");
-
-        this.stringTable = new String_Table(this.project.getCachePath());
-
-        Split(["#main-sidebar-1", "#main-sidebar-2", "#main-sidebar-3"], {
-            sizes: [15, 65, 20],
-            minSize: 0,
-            snapOffset: 60,
-            gutterSize: 12
-        });
-
-        this.initTools();
-    }
-
-    initTools() {
-        this.node.querySelector(".data-tool-save").onclick = () => this.save();
-        this.node.querySelector(".data-tool-openFieldDir").onclick = () => {
-            electron.shell.showItemInFolder(
-                this.fieldEditor.getFieldFilePath()
-            );
-        };
-
-        this.node.querySelector(".data-tool-packStatic").onclick = () =>
-            this.packStatic();
-        this.node.querySelector(".data-tool-openTitleBgDir").onclick = () => {
-            electron.shell.showItemInFolder(
-                path.join(this.project.getPath(), "TitleBG.pack")
-            );
-        };
-
-        this.node.querySelector(".data-tool-addActorStatic").onclick =
-            async () => {
-                this.fieldEditor.actorHandler.addFromData(
-                    ActorParams.createTemplate(
-                        "FldObj_HugeMazeTorchStand_A_01"
-                    ),
-                    "Static"
-                );
-            };
-
-        this.node.querySelector(".data-tool-addActorDyn").onclick =
-            async () => {
-                this.fieldEditor.actorHandler.addFromData(
-                    ActorParams.createTemplate(
-                        "FldObj_HugeMazeTorchStand_A_01"
-                    ),
-                    "Dynamic"
-                );
-            };
-
-        Actor_Templates.getHtmlSelect().then(
-            html =>
-                (this.node.querySelector(".data-tool-actorTemplate").innerHTML =
-                    html)
-        );
-
-        this.node.querySelector(".data-tool-addActorTemplate").onclick =
-            async () => {
-                const templateName = this.node.querySelector(
-                    ".data-tool-actorTemplate"
-                ).value;
-                this.fieldEditor.actorHandler.addFromTemplate(templateName);
-            };
-    }
-
-    /**
-     * saves the field
-     */
-    async save() {
-        await this.loader.setStatus("Saving Field");
-        await this.loader.show();
-
-        await this.fieldEditor.save();
-
-        await this.loader.hide();
-        Notify.success(`Field '${this.fieldSection}' saved`);
-    }
-
-    async packStatic() {
-        await this.loader.setStatus("Packing static maps");
-        await this.loader.show();
-
-        await this.fieldEditor.titleBgHandler.pack();
-
-        await this.loader.hide();
-        Notify.success(`Static maps packed`);
-    }
-
-    async openField(fieldSection, fieldPos = undefined) {
-        this.fieldSection = fieldSection;
-
-        await this.loader.show();
-        await this.loader.setStatus("Loading Field");
-        try {
-            this.stringTable.loader = this.loader;
-            //await this.stringTable.load(); // not needed now, yay!
-
-            if (typeof global.gc == "function")
-                // free some memory after maybe loading the stringtable
-                global.gc();
-
-            this.fieldDir = path.join(
-                this.project.getFieldPath("data"),
-                this.fieldSection
-            );
-            const alreadyOpened = await fs.pathExists(this.fieldDir);
-            await fs.ensureDir(this.fieldDir);
-
-            await this.loader.setStatus("Extracting Static Maps");
-            await extractStaticMubins(
-                this.config.getValue("game.aocPath") ||
-                    this.config.getValue("game.basePath"),
-                this.project.getPath(),
-                this.fieldDir,
-                this.fieldSection
-            );
-
-            if (!alreadyOpened) {
-                await extractField(
-                    this.fieldGamePath,
-                    this.fieldStaticGamePath,
-                    this.fieldDir,
-                    this.fieldSection
-                );
-            }
-
-            await this.loader.setStatus("Loading Field");
-            await this.fieldEditor.load(
-                this.fieldDir,
-                this.fieldSection,
-                fieldPos
-            );
-
-            this.render();
-
-            // some performance cuts
-            this.fieldEditor.setRenderSetting("targetFPS", "number", 30);
-            this.fieldEditor.setRenderSetting("camSpeed", "number", 2);
-            //this.fieldEditor.setRenderSetting("accurateTimer", "bool", true);
-        } catch (e) {
-            await this.loader.hide();
-            console.log(e);
-            throw e;
-        }
-
-        await this.loader.hide();
-    }
-
-    render() {
-        this.footerNode.innerHTML = "Loaded Field-Section: " + this.fieldDir;
-
-        this.node.querySelector(".data-field-section").innerHTML =
-            this.fieldSection;
-
-        if (
-            this.fieldEditor.actorHandler.dataActorStatic &&
-            this.fieldEditor.actorHandler.dataActorStatic.Objs
+    this.isAoc = !!this.config.getValue("game.aocPath");
+    this.fieldGamePath = this.isAoc
+      ? path.join(
+          this.config.getValue("game.aocPath"),
+          "content",
+          "0010",
+          "Map",
+          "MainField"
         )
-            this.node.querySelector(".data-actors-staticCount").innerHTML =
-                this.fieldEditor.actorHandler.dataActorStatic.Objs.length;
+      : path.join(
+          this.config.getValue("game.updatePath"),
+          "content",
+          "Map",
+          "MainField"
+        );
+    this.fieldStaticGamePath = path.join(
+      this.config.getValue("game.updatePath"),
+      "content",
+      "Physics",
+      "StaticCompound",
+      "MainField"
+    );
 
-        if (
-            this.fieldEditor.actorHandler.dataActorDyn &&
-            this.fieldEditor.actorHandler.dataActorDyn.Objs
-        )
-            this.node.querySelector(".data-actors-dynamicCount").innerHTML =
-                this.fieldEditor.actorHandler.dataActorDyn.Objs.length;
+    this.fieldDir = null;
+    this.fieldSection = null;
 
-        let prodNum = 0;
-        for (const prodSection of this.fieldEditor.actorHandler.dataActorProd) {
-            this.node.querySelector(
-                ".data-actors-prodCount-" + prodNum++
-            ).innerHTML = prodSection.length;
-        }
+    this.footerNode = footer.querySelector(".data-footer");
 
-        this.fieldEditor.start();
-    }
+    this.stringTable = new String_Table(this.project.getCachePath());
 
-    async run() {
-        await super.run();
+    Split(["#main-sidebar-1", "#main-sidebar-2", "#main-sidebar-3"], {
+      sizes: [15, 65, 20],
+      minSize: 0,
+      snapOffset: 60,
+      gutterSize: 12,
+    });
 
-        let staticHandler;
-        console.log(this);
-        if (this.isAoc) {
-            new AocMainField_Handler(
-                this.config.getValue("game.aocPath"),
-                this.project.getPath()
-            );
-            console.log("Aoc handler");
+    this.initTools();
+  }
+
+  initTools() {
+    this.node.querySelector(".data-tool-save").onclick = () => this.save();
+    this.node.querySelector(".data-tool-openFieldDir").onclick = () => {
+      electron.shell.showItemInFolder(this.fieldEditor.getFieldFilePath());
+    };
+    this.node.querySelector(".data-tool-buildTemplate").onclick = () =>
+      this.buildTemplate();
+
+    this.node.querySelector(".data-tool-packStatic").onclick = () =>
+      this.packStatic();
+    this.node.querySelector(".data-tool-openTitleBgDir").onclick = () => {
+      electron.shell.showItemInFolder(
+        path.join(this.project.getPath(), "TitleBG.pack")
+      );
+    };
+
+    this.node.querySelector(".data-tool-addActorStatic").onclick = async () => {
+      this.fieldEditor.actorHandler.addFromData(
+        ActorParams.createTemplate("FldObj_HugeMazeTorchStand_A_01"),
+        "Static"
+      );
+    };
+
+    this.node.querySelector(".data-tool-addActorDyn").onclick = async () => {
+      this.fieldEditor.actorHandler.addFromData(
+        ActorParams.createTemplate("FldObj_HugeMazeTorchStand_A_01"),
+        "Dynamic"
+      );
+    };
+
+    Actor_Templates.getHtmlSelect().then(
+      (html) =>
+        (this.node.querySelector(".data-tool-actorTemplate").innerHTML = html)
+    );
+
+    this.node.querySelector(".data-tool-addActorTemplate").onclick =
+      async () => {
+        const templateName = this.node.querySelector(
+          ".data-tool-actorTemplate"
+        ).value;
+        this.fieldEditor.actorHandler.addFromTemplate(templateName);
+      };
+  }
+
+  /**
+   * Template Builder
+   * Im aware this code is crap but ill fix it later.
+   */
+  async buildTemplate() {
+    await this.loader.setStatus("Building Template");
+    await this.loader.show();
+
+    if (this.fieldEditor.actorEditor.selectedActors == 0) {
+      await this.loader.hide();
+      Notify.error("Nothing Selected", "Template Builder Failed");
+    } else {
+      var JSONObject;
+      var JSONArray = [];
+      var hashIds = [];
+      var initX;
+      var initY;
+      var initZ;
+
+      for (
+        var i = 0;
+        i < this.fieldEditor.actorEditor.selectedActors.length;
+        i++
+      ) {
+        JSONObject = JSON.parse(
+          this.fieldEditor.actorEditor.selectedActors[i].getParamJSON()
+        );
+        if (i == 0) {
+          initX = JSONObject["Translate"][0]["value"];
+          JSONObject["Translate"][0]["value"] = 0;
+          initY = JSONObject["Translate"][1]["value"];
+          JSONObject["Translate"][1]["value"] = 0;
+          initZ = JSONObject["Translate"][2]["value"];
+          JSONObject["Translate"][2]["value"] = 0;
         } else {
-            new TitleBG_Handler(
-                this.config.getValue("game.updatePath"),
-                this.project.getPath()
-            );
-            console.log("TitleBG handler");
+          JSONObject["Translate"][0]["value"] =
+            JSONObject["Translate"][0]["value"] - initX;
+          JSONObject["Translate"][1]["value"] =
+            JSONObject["Translate"][1]["value"] - initY;
+          JSONObject["Translate"][2]["value"] =
+            JSONObject["Translate"][2]["value"] - initZ;
         }
-        this.fieldEditor = new Field_Editor(
-            this.node.querySelector(".shrine-canvas"),
-            this.node,
-            this.project,
-            this.loader,
-            this.stringTable,
-            staticHandler
-        );
+        var id = "{ID" + i + "}";
+        hashIds[i] = {
+          HashId: JSONObject["HashId"]["value"],
+          id: id,
+        };
+        JSONObject["HashId"]["value"] = id;
+        JSONArray.push(JSONObject);
+      }
 
-        let fieldSection = this.args.section ? this.args.section : "J-8";
-        let fieldPos = this.args.pos ? this.args.pos : undefined;
-
-        if (fieldPos) {
-            fieldPos[0] = parseFloat(fieldPos[0]);
-            fieldPos[1] = parseFloat(fieldPos[1]);
+      for (var j = 0; j < JSONArray.length; j++) {
+        if ("LinksToObj" in JSONArray[j]) {
+          for (var k = 0; k < JSONArray[j]["LinksToObj"].length; k++) {
+            if (
+              !hashIds.some((item) => {
+                if (
+                  item["HashId"] ==
+                  JSONArray[j]["LinksToObj"][k]["DestUnitHashId"]["value"]
+                ) {
+                  JSONArray[j]["LinksToObj"][k]["DestUnitHashId"]["value"] =
+                    item["id"];
+                  return true;
+                } else {
+                  return false;
+                }
+              })
+            ) {
+              JSONArray[j]["LinksToObj"][k]["DestUnitHashId"]["value"] =
+                "Unknown";
+            }
+          }
         }
+      }
 
-        await this.openField(fieldSection, fieldPos);
+      var name = document.getElementById("templateInput").value;
+
+      name = this.snakeCase(name);
+
+      var actor = {
+        name: name,
+        actors: JSONArray,
+      };
+
+      if (name == "") {
+        await this.loader.hide();
+        Notify.error("Invalid Name", "Template Builder Failed");
+      } else {
+        clipboard.writeText(JSON.stringify(actor, null, 2));
+        await this.loader.hide();
+        Notify.success(name + " : Copied to Clipboard", "Template Built");
+      }
+      //TODO someday make it generate the template.json in the correct location
     }
+  }
+
+  /**
+   * Not mine IDK where I got this.
+   */
+  snakeCase = (string) => {
+    const toSnakeCase = (str = "") => {
+      const strArr = str.split(" ");
+      const snakeArr = strArr.reduce((acc, val) => {
+        return acc.concat(val.toLowerCase());
+      }, []);
+      return snakeArr.join("_");
+    };
+    const newText = toSnakeCase(string);
+    return newText;
+  };
+
+  /**
+   * saves the field
+   */
+  async save() {
+    await this.loader.setStatus("Saving Field");
+    await this.loader.show();
+
+    await this.fieldEditor.save();
+
+    await this.loader.hide();
+    Notify.success(`Field '${this.fieldSection}' saved`);
+  }
+
+  async packStatic() {
+    await this.loader.setStatus("Packing static maps");
+    await this.loader.show();
+
+    await this.fieldEditor.titleBgHandler.pack();
+
+    await this.loader.hide();
+    Notify.success(`Static maps packed`);
+  }
+
+  async openField(fieldSection, fieldPos = undefined) {
+    this.fieldSection = fieldSection;
+
+    await this.loader.show();
+    await this.loader.setStatus("Loading Field");
+    try {
+      this.stringTable.loader = this.loader;
+      //await this.stringTable.load(); // not needed now, yay!
+
+      if (typeof global.gc == "function")
+        // free some memory after maybe loading the stringtable
+        global.gc();
+
+      this.fieldDir = path.join(
+        this.project.getFieldPath("data"),
+        this.fieldSection
+      );
+      const alreadyOpened = await fs.pathExists(this.fieldDir);
+      await fs.ensureDir(this.fieldDir);
+
+      await this.loader.setStatus("Extracting Static Maps");
+      await extractStaticMubins(
+        this.config.getValue("game.aocPath") ||
+          this.config.getValue("game.basePath"),
+        this.project.getPath(),
+        this.fieldDir,
+        this.fieldSection
+      );
+
+      if (!alreadyOpened) {
+        await extractField(
+          this.fieldGamePath,
+          this.fieldStaticGamePath,
+          this.fieldDir,
+          this.fieldSection
+        );
+      }
+
+      await this.loader.setStatus("Loading Field");
+      await this.fieldEditor.load(this.fieldDir, this.fieldSection, fieldPos);
+
+      this.render();
+
+      // some performance cuts
+      this.fieldEditor.setRenderSetting("targetFPS", "number", 30);
+      this.fieldEditor.setRenderSetting("camSpeed", "number", 2);
+      //this.fieldEditor.setRenderSetting("accurateTimer", "bool", true);
+    } catch (e) {
+      await this.loader.hide();
+      console.log(e);
+      throw e;
+    }
+
+    await this.loader.hide();
+  }
+
+  render() {
+    this.footerNode.innerHTML = "Loaded Field-Section: " + this.fieldDir;
+
+    this.node.querySelector(".data-field-section").innerHTML =
+      this.fieldSection;
+
+    if (
+      this.fieldEditor.actorHandler.dataActorStatic &&
+      this.fieldEditor.actorHandler.dataActorStatic.Objs
+    )
+      this.node.querySelector(".data-actors-staticCount").innerHTML =
+        this.fieldEditor.actorHandler.dataActorStatic.Objs.length;
+
+    if (
+      this.fieldEditor.actorHandler.dataActorDyn &&
+      this.fieldEditor.actorHandler.dataActorDyn.Objs
+    )
+      this.node.querySelector(".data-actors-dynamicCount").innerHTML =
+        this.fieldEditor.actorHandler.dataActorDyn.Objs.length;
+
+    let prodNum = 0;
+    for (const prodSection of this.fieldEditor.actorHandler.dataActorProd) {
+      this.node.querySelector(".data-actors-prodCount-" + prodNum++).innerHTML =
+        prodSection.length;
+    }
+
+    this.fieldEditor.start();
+  }
+
+  async run() {
+    await super.run();
+
+    let staticHandler;
+    console.log(this);
+    if (this.isAoc) {
+      new AocMainField_Handler(
+        this.config.getValue("game.aocPath"),
+        this.project.getPath()
+      );
+      console.log("Aoc handler");
+    } else {
+      new TitleBG_Handler(
+        this.config.getValue("game.updatePath"),
+        this.project.getPath()
+      );
+      console.log("TitleBG handler");
+    }
+    this.fieldEditor = new Field_Editor(
+      this.node.querySelector(".shrine-canvas"),
+      this.node,
+      this.project,
+      this.loader,
+      this.stringTable,
+      staticHandler
+    );
+
+    let fieldSection = this.args.section ? this.args.section : "J-8";
+    let fieldPos = this.args.pos ? this.args.pos : undefined;
+
+    if (fieldPos) {
+      fieldPos[0] = parseFloat(fieldPos[0]);
+      fieldPos[1] = parseFloat(fieldPos[1]);
+    }
+
+    await this.openField(fieldSection, fieldPos);
+  }
 };
